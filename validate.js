600ab286a5f35c98f2119e32bbd75bd25df54d82
diana <diana.vile@gmail.com>
2020-10-29 12:32:59 +0100

    add register form

--- validate.js
+++ validate.js
@@ -11,8 +11,7 @@ function searchForm() {
 	let search = document.forms["myForm"]["search"];//name (form), id (input search)
     
 	//condicionales
-	//innerHTML = texto assignado por un variable
-	//textContent = texto directo
+	//"innerHTML" = texto assignado por un variable VS "textContent" = texto directo
 	if (search.value == "") {
 		search.classList.add("is-invalid"); //add invalid list if search is empty
 		document.getElementById("errorSearch").textContent = "Required field";
@@ -32,77 +31,82 @@ function searchForm() {
 		return true;
 	}
 }
-	
-/*
+
 function registerForm() {
 	let acumErrores = 0;
 	form.classList.remove('is-invalid');
-	
-	let inputEmail = document.getElementById('inputEmail');
-    let inputPassword = document.forms["myForm"]["inputPassword"];
-   // let newPassword = document.forms["myForm"]["newPassword"];
-    //let currentPassword = document.forms["myForm"]["currentPassword"];
-	let inputAddress = document.forms["myForm"]["inputAddress"];
-	let inputProvince = document.forms["myForm"]["inputProvince"];
-	let inputCity = document.forms["myForm"]["inputCity"];
-	let inputZip = document.forms["myForm"]["inputZip"];
-	let gridCheck = document.forms["myForm"]["gridCheck"];
 
-    if (inputEmail.value == "") {
+	//let mail = document.getElementById('inputEmail');
+	let inputEmail = document.forms["myForm2"]["mail"];
+    let newPassword = document.forms["myForm2"]["newPassword"];
+    let repeatPassword = document.forms["myFormId2"]["repeatPassword"];
+	let inputAddress = document.forms["myForm2"]["inputAddress"];
+	let inputProvince = document.forms["myForm2"]["inputProvince"];
+	let inputCity = document.forms["myForm2"]["inputCity"];
+	let inputZip = document.forms["myForm2"]["inputZip"];
+	let gridCheck = document.forms["myForm2"]["gridCheck"];
+
+	 if (inputEmail.value == "") {
         inputEmail.classList.add("is-invalid");
-        document.getElementById("errorEmail").textContent = "This field is required";
-        acumErrores++;
+        document.getElementById("errorEmail").textContent = "Required field";
+		console.log("Required field");
+		acumErrores++;
+		console.log(acumErrores);
     } else if (!validar_email(inputEmail.value)) {
         inputEmail.classList.add("is-invalid");
-        document.getElementById("errorEmail").textContent = "The email does not meet required mailformat";
-        acumErrores++;
-    } else {
-        inputEmail.classList.add("is-valid");
-        document.getElementById("successEmail").textContent = "This email is correct";
-     }
-
+        document.getElementById("errorEmail").textContent = "The email does not meet required format";
+		console.log("The email does not meet required format");
+		acumErrores++;
+		console.log(acumErrores);
+    } 
     if(inputPassword.value == "") {
 		inputPassword.classList.add("is-invalid");
-		document.getElementById("errorPassword").textContent = "This field is required";
-		acumErrores ++;
+		document.getElementById("errorPassword").textContent = "Required field";
+		console.log("Required field");
+		acumErrores++;
+		console.log(acumErrores);
 	}
-	
     if(inputAddress.value == "") {
 		inputAddress.classList.add("is-invalid");
-		document.getElementById("errorAddress").textContent = "This field is required";
-		acumErrores ++;
+		document.getElementById("errorAddress").textContent = "Required field";
+		console.log("Required field");
+		acumErrores++;
+		console.log(acumErrores);
 	}
-
     if(inputProvince.value == "") {
 		inputProvince.classList.add("is-invalid");
 		document.getElementById("errorProvince").textContent = "The province is required";
-		acumErrores ++;
-	}
-	
+		console.log("The province is required");
+		acumErrores++;
+		console.log(acumErrores);
+	}	
 	if(inputCity.value == "") {
 		inputCity.classList.add("is-invalid");
 		document.getElementById("errorCity").textContent = "The city is missing";
-		acumErrores ++;
-	}
-	
+		console.log("The city is missing");
+		acumErrores++;
+		console.log(acumErrores);
+	}	
 	if(inputZip.value == "" || inputZip.length!=5) {
 		inputZip.classList.add("is-invalid");
 		document.getElementById("errorZip").textContent = "The zipcode does not meet the required format.";
-		acumErrores ++;
-	}
-	
+		console.log("The zipcode does not meet the required format.");
+		acumErrores++;
+		console.log(acumErrores);
+	}	
 	if(!gridCheck.checked) {
 		gridCheck.classList.add("is-invalid");
 		document.getElementById("errorCheck").textContent = "I accept the terms & conditions";
-		acumErrores ++;
+		console.log("I accept the terms & conditions");
+		acumErrores++;
+		console.log(acumErrores);
 	}
-
-    if (acumErrores > 0){
+	  if (acumErrores > 0){
         return false;
     } else{
 		return true;
-	}
-}*/
+	}	
+}
 
 //removes invalid color from input, when valid
 //form.addEventListener('blur', (event) => {
